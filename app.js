5f7894845e3bd95843b07a298efd65656c736350
Thauan Rafael <thauanrafaeltrabalho@gmail.com>
2024-02-16 09:50:16 -0300

    First design

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-const express = require('express')
-const app = express()
-const https = require('https')
+const express = require('express');
+const app = express();
+const https = require('https');
 
-app.use(express.urlencoded({ extended: true}))
-app.use(express.static('public'))
+app.set('view engine','ejs');
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
 
-app.get('/', (req,res) => {res.sendFile(__dirname + '/index.html') })
-app.listen(3000, () => {console.log('App running on port 3000')})
+app.get('/', (req,res) => {
+  let weatherInfo = false
+  res.render('weather',{weatherInfo})
+});
+app.listen(3000, () => {console.log('App running on port 3000')});
 
 app.post('/', function(req,res){
   
     const {latitude, longitude} = req.body;
-    const appId = '7e9f5db2e73b7f2c43c3354263178440'
-    const url = 'https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid='+appId+'&units=metric'
+    const appId = '7e9f5db2e73b7f2c43c3354263178440';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid='+appId+'&units=metric';
     https.get(url, function(response) {
       response.on('data', function(data) {
-          const weatherData = JSON.parse(data)
-          const temperature = weatherData.main.temp
-          const description = weatherData.weather[0].description
-          const icon = weatherData.weather[0].icon
-          const imgUrl = 'https://openweathermap.org/img/wn/' + icon + '.png'
-          res.write('<h1>'+weatherData.name+' temperature is '+temperature+' Celsius</h1>')
-          res.write('<h3>Description of time: '+description+'</h3>')
-          res.write('<img src='+imgUrl+' />')
-          res.send()
+          const weatherData = JSON.parse(data);
+          const icon = weatherData.weather[0].icon;
+          let weatherInfo = {
+            name: weatherData.name,
+            temperature: weatherData.main.temp,
+            description: weatherData.weather[0].description,
+            icon: weatherData.weather[0].icon,
+          }
+          res.render('weather',{weatherInfo})
       })
     })
   }
